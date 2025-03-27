1962e279a35cf441a2dec15984aadb6ce8410195
jomspangilinan <pangilinanjohnjomel@gmail.com>
2025-03-27 10:32:36 +0800

    update interface

--- src/components/mobileWeeklyCalendar.tsx
+++ src/components/mobileWeeklyCalendar.tsx
@@ -49,7 +49,7 @@ export default function WeeklyCalendar({
 
   return (
     <div
-    className="bg-white shadow-md mx-4 rounded-lg text-center px-4 py-4 relative z-10 mb-4 mt-[-50px]">
+      className="bg-white shadow-md mx-4 rounded-lg text-center px-4 py-4 relative z-10 mb-4 mt-[-50px]">
       {/* Month and Year */}
       <p className={`text-lg font-bold ${selectedTheme.split(' ')[0]} mb-4`}>
         {format(startOfCurrentWeek, 'MMMM yyyy')}
@@ -71,16 +71,15 @@ export default function WeeklyCalendar({
             <div
               key={day.toISOString()}
               onClick={() => setSelectedDate(format(day, 'yyyy-MM-dd'))}
-              className={`relative flex flex-col items-center justify-center w-14 h-18 cursor-pointer rounded-full transition-all duration-200 ${
-                isSameDay(day, new Date(selectedDate))
-                  ? `${selectedTheme} text-white font-bold`
-                  : 'text-gray-700 hover:bg-gray-200'
-              }`}
+              className={`relative flex flex-col items-center justify-center w-14 h-18 cursor-pointer rounded-full transition-all duration-200 ${isSameDay(day, new Date(selectedDate))
+                ? `${selectedTheme} text-white font-bold`
+                : 'text-gray-700 hover:bg-gray-200'
+                }`}
             >
               {/* Circle for Today */}
               {isToday(day) && (
                 <div className="absolute bottom-1 w-2 h-2 rounded-full"
-                style={{ backgroundColor: '#FF8989' }}/>
+                  style={{ backgroundColor: '#FF8989' }} />
               )}
               <p className="text-xs font-medium">{format(day, 'EEE')}</p>
               <p className="text-sm font-semibold">{format(day, 'd')}</p>
@@ -89,14 +88,13 @@ export default function WeeklyCalendar({
         </div>
 
         {/* Next Button */}
-        {!daysOfWeek.some((day) => isToday(day)) && (
-          <button
-            onClick={handleNextWeek}
-            className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
-          >
-            <FaChevronRight className="text-lg text-gray-600" />
-          </button>
-        )}
+        <button
+          onClick={handleNextWeek}
+          className={`w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300 transition-opacity duration-200 ${daysOfWeek.some((day) => isToday(day)) ? 'invisible' : 'visible'
+            }`}
+        >
+          <FaChevronRight className="text-lg text-gray-600" />
+        </button>
       </div>
     </div>
   );
